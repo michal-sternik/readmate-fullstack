6fc3220747e01e7351b1705f5ba16d511da9ecdf
Michał Sternik <64137088+michal-sternik@users.noreply.github.com>
2025-05-24 14:38:08 +0200

    Update auth.controller.ts

--- readmate-backend/src/auth/auth.controller.ts
+++ readmate-backend/src/auth/auth.controller.ts
@@ -63,7 +63,7 @@ export class AuthController {
       res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24,
       });
       return { message: 'User logged in' };
